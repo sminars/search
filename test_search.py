6ffc353430546c8bc423ec1bd642519f54e04496
mciethan <emaclex@gmail.com>
2022-04-23 12:52:05 -0400

    added a test class and initial test

--- test_search.py
+++ test_search.py
@@ -2,8 +2,17 @@ import pytest
 from index import *
 
 def test_word_processing():
+    """ensure that Indexer adds the words to the corpus that we'd expect"""
     ind = Indexer(["small_test_wiki.xml", "", "", ""])
-    print(ind.corpus)
-    assert len(ind.corpus) == 27
+
+    expected_corpus = ['juic', 'york', 'blood', 'love', 'mani', 'sport', 
+                        'england', 'build', 'popular', 'kitchen', 'peopl', 
+                        'billiard', 'tall', 'aluminum', 'live', 'categori', 
+                        'use', 'orang', 'citi', 'type', 'commut', 'make', 'nyc',
+                        'made', 'new', 'skyscrap', 'foil']
+    
+    assert len(ind.corpus) == len(expected_corpus)
+    for word in expected_corpus:
+        assert word in ind.corpus
     
     
