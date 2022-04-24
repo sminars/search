652c2a25642281197e8e9b325a31a0fdb2a84438
mciethan <emaclex@gmail.com>
2022-04-24 14:49:32 -0400

    indexer draft 1 complete

--- index.py
+++ index.py
@@ -2,45 +2,67 @@
 from ctypes import ArgumentError
 import sys
 import re
+import math
 import xml.etree.ElementTree as et
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+import file_io
 
 class Indexer:
     def __init__(self, args):
         if len(args) != 4:
             raise ArgumentError("Must have exactly 4 arguments after index.py -- try again.")
-    
-        root: "Element" = et.parse(args[0]).getroot()
-        self.all_pages: "ElementTree" = root.findall("page")
-        self.n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
-        self.STOP_WORDS = set(stopwords.words('english'))
-        self.stemmer = PorterStemmer()
-        self.corpus = {} # keys = words, values = indices
-        self.index = 0
-        self.word_counts = []
-        self.make_corpus(self.all_pages)
-       
-       
-      
+        
+        xml_file = args[0]
+        title_file = args[1]
+        docs_file = args[2]
+        words_file = args[3]
 
-    def add_to_corpus(self, words: str, info):
+        root: "Element" = et.parse(xml_file).getroot()
+        self.page_ids = []  # list of tuples with (page ID, page title)
+        self.n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
+        self.corpus = {} # keys = words, values = indices
+        self.corpus_index = 0  
+        self.tf_array = []
+        self.idf_array = []
+        self.relevance = []
+        self.make_corpus(root.findall("page"))
+        self.calculate_relevance()
+        self.write_files(title_file, words_file)
+          
+    def add_to_corpus(self, words: str, info: list, idf: list):
         """tokenizes, lower cases, and stems the elements of words and adds non-
-        stop words to self.corpus"""
+        stop words to self.corpus
+        
+        parameters:
+        info - two-element list where index 0 is a row of word counts and index 1 is an
+        int representing the current maximum word count in that row
+        """
+        STOP_WORDS = set(stopwords.words('english'))
+        stemmer = PorterStemmer()
         tokens = re.findall(self.n_regex, words)
+
         for wrd in tokens:
-            if wrd.lower() not in self.STOP_WORDS:
-                stemmed_wrd = self.stemmer.stem(wrd.lower())
+            if wrd.lower() not in STOP_WORDS:
+                stemmed_wrd = stemmer.stem(wrd.lower())
                 if stemmed_wrd not in self.corpus:
-                    self.corpus[stemmed_wrd] = self.index
-                    self.index += 1
+                    self.corpus[stemmed_wrd] = self.corpus_index
+                    self.corpus_index += 1
                     # also add a new column in page row and in word_counts
                     info[0].append(1)
-                    for row in self.word_counts:
+                    idf.append(1)
+                    for row in self.tf_array:
                         row.append(0)
-                else:
+                else:  # the word is in the corpus
+                    # increment the word count for this word in this page
                     info[0][self.corpus[stemmed_wrd]] += 1
-                    # update maximum value of this row
+
+                    # if this is the first time we've seen this word in this row
+                    if info[0][self.corpus[stemmed_wrd]] == 1:
+                        # increment this word's idf n_i count
+                        idf[self.corpus[stemmed_wrd]] += 1
+
+                # update maximum value of this row
                 if info[0][self.corpus[stemmed_wrd]] > info[1]:
                     info[1] = info[0][self.corpus[stemmed_wrd]]
         
@@ -48,13 +70,14 @@ class Indexer:
     def make_corpus(self, pages : list):
         """takes in xml pages and populates global variable corpus"""        
        
-        ids = []
         id2pn = {}  # page ids to list of names of pages linked to
-        max_in_each_row = []
+        max_in_each_row = [] # number of times most frequent word appears for each page
+        idf_arr = [] # same length as corpus, keeps track of n_i counts
 
         for page in pages:
             pid = int(page.find('id').text.strip())
-            ids.append(pid)
+            title = page.find('title').text.strip()
+            self.page_ids.append((pid, title))
             id2pn[pid] = []
             page_row = [0] * len(self.corpus)
             current_row_max = 0
@@ -66,39 +89,55 @@ class Indexer:
                 if re.match('\[\[[^\[]+?\]\]', word):
                     w = word[2:-2]
                     if '|' in w:
-                        link_page, link_text = w.split('|')
+                        link_page, link_text = w.split('|', 1)
                         id2pn[pid].append(link_page.lower())
-                        self.add_to_corpus(link_text, page_info)
+                        self.add_to_corpus(link_text, page_info, idf_arr)
                     elif ':' in w:
-                        id2pn[pid].append(w)
+                        id2pn[pid].append(w)  # we aren't sure if category pages should count as links
                         w = w.replace(":", " ")
-                        self.add_to_corpus(w, page_info)
+                        self.add_to_corpus(w, page_info, idf_arr)
                     else:  # links where the text is the name of the page
                         id2pn[pid].append(w)
-                        self.add_to_corpus(w, page_info)
-                # elif word.lower() not in STOP_WORDS:
-                #     self.corpus.add(stemmer.stem(word.lower()))
-                else:
-                    self.add_to_corpus(word, page_info)
+                        self.add_to_corpus(w, page_info, idf_arr)
+                else:  # i.e. if the word is not a link
+                    self.add_to_corpus(word, page_info, idf_arr)
 
-            self.word_counts.append(page_info[0])
+            self.tf_array.append(page_info[0])
             max_in_each_row.append(page_info[1])
                 
-
-       # print(id2pn)
-      #  print(ids)
-        # print("the" in STOP_WORDS)
-        #print(self.corpus)
-        for row in self.word_counts:
-            print(row)
-        print(max_in_each_row)
         self.calculate_tf(max_in_each_row)
-        for row in self.word_counts:
-            print(row)
+        self.calculate_idf(idf_arr)
+        # self.calculate_relevance()
+        # self.write_files()
 
     def calculate_tf(self, maxs: list):
-        for idx, row in enumerate(self.word_counts):
-            self.word_counts[idx] = [wc / maxs[idx] for wc in row]
+        for idx, row in enumerate(self.tf_array):
+            self.tf_array[idx] = [wc / maxs[idx] for wc in row]
+
+    def calculate_idf(self, n_i_list: list):
+        n = len(self.page_ids)
+        self.idf_array = [math.log(n / n_i) for n_i in n_i_list]
+    
+    def calculate_relevance(self):
+        for r in self.tf_array:
+            self.relevance.append([elem * self.idf_array[idx] for idx, elem in enumerate(r)])
+
+    def write_files(self, title_file: str, words_file: str):
+        words_to_relevance = {}
+
+        for col_idx, word in enumerate(self.corpus.keys()):
+            ids_to_relevance = {}
+            for row_idx, row in enumerate(self.relevance):
+                page_id = self.page_ids[row_idx][0]
+                ids_to_relevance[page_id] = self.relevance[row_idx][col_idx]
+
+            words_to_relevance[word] = ids_to_relevance
+
+        # pass words_to_relevance into file_io to write to words file
+        file_io.write_words_file(words_file, words_to_relevance)
+        # convert page_ids to dictionary and write to title file
+        file_io.write_title_file(title_file, dict(self.page_ids))
+
 
 if __name__ == "__main__":
     idxr = Indexer(sys.argv[1:])
--- test_search.py
+++ test_search.py
@@ -19,5 +19,52 @@ def test_arguments():
     with pytest.raises(ArgumentError):
         Indexer(["1", "2", "3"])
         Indexer(["1", "2", "3", "4", "5"])
+
+def test_tf():
+    """tests that expected term frequencies on a tiny wiki match the code output
+    values, and that the linked pages of pipe links are not included in the 
+    corpus and not counted in term frequencies."""
     
+    expected_corpus = [
+        'york', 'mani', 'sport', 
+        'england', 'popular', 
+        'billiard', 'tall', 'categori', 
+        'citi', 'new', 'build'
+    ]
     
+    ind = Indexer(["pipelink_tiny_wiki.xml", "", "", ""])
+
+    assert len(ind.corpus) == len(expected_corpus)
+    for word in expected_corpus:
+        assert word in ind.corpus
+
+    expected_tf = [
+        [1.0, 1.0, 0.5, 0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    ]
+
+   
+
+    assert ind.tf_array == expected_tf
+    
+
+def test_idf():
+    ind_tiny = Indexer(["pipelink_tiny_wiki.xml", "", "", ""])
+    expected_idf = [math.log(2)] * 11
+    assert ind_tiny.idf_array == expected_idf
+
+    ind_test_idf = Indexer(["test_idf_wiki.xml", "", "", ""])
+    expected = [math.log(2), math.log(4/3), math.log(2), math.log(2)]
+    assert ind_test_idf.idf_array == expected
+
+def test_relevance():
+    ind_test_idf = Indexer(["test_idf_wiki.xml", "", "", ""])
+    expected_rel = [
+        [math.log(2), math.log(4/3), 0, 0],
+        [0, 0, math.log(2), math.log(2)],
+        [0, math.log(4/3), math.log(2)/2, 0],
+        [math.log(2)/2, math.log(4/3)/2, 0, math.log(2)]
+    ]
+    assert ind_test_idf.relevance == expected_rel
+
+
