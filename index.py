d8d41e125c4f0a83ab7992299dc2c5beb4d9f5ce
mciethan <emaclex@gmail.com>
2022-04-26 21:16:01 -0400

    reorganized indexer to go faster

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ import file_io
 class IndexInfo:
     def __init__(self, idf_arr, max_per_row, word_counts):
         self.idf_array = idf_arr
-        self.max_frequency_per_row = max_per_row
+        self.max_per_row = max_per_row
         self.word_counts = word_counts
 
 
@@ -20,31 +20,14 @@ class Indexer:
         if len(args) != 4:
             raise ArgumentError("Must have exactly 4 arguments after index.py -- try again.")
         
-        xml_file = args[0]
-        self.title_file = args[1]
-        self.docs_file = args[2]
-        self.words_file = args[3]
-
-        root: "Element" = et.parse(xml_file).getroot()
         self.page_ids = []  # list of tuples with (page ID, page title)
         self.n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
         self.corpus = {} # keys = words, values = indices
         self.corpus_index = 0  
-        self.tf_array = []
-        self.idf_array = []
-        self.relevance = []
 
-        # ii = self.make_corpus(root.findall("page"))
-        # rel_array = self.make_relevance(ii)
-        # words_to_rel = self.make_wtr_dict(rel_array)
-
-        self.make_corpus(root.findall("page"))
-        self.calculate_relevance()
-        self.write_files(self.title_file, self.words_file)
-
-        # should creating an indexer do everything, or just initialize the vars?
+        self.write_files(args[0], args[1], args[3])
           
-    def add_to_corpus(self, words: str, info: list, idf: list):
+    def add_to_corpus(self, words: str, info: list, idf: list, word_counts: list):
         """tokenizes, lower cases, and stems the elements of words and adds non-
         stop words to self.corpus
         
@@ -67,7 +50,7 @@ class Indexer:
                     # also add a new column in page row and in word_counts
                     info[0].append(1)
                     idf.append(1)
-                    for row in self.tf_array:
+                    for row in word_counts:
                         row.append(0)
                 else:  # the word is in the corpus
                     # increment the word count for this word in this page
@@ -88,7 +71,8 @@ class Indexer:
        
         id2pn = {}  # page ids to list of names of pages linked to
         max_in_each_row = [] # keeps track of frequency of most frequent word for each page
-        idf_arr = [] # keeps track of n_i counts for each word in corpus
+        ni_counts = [] # keeps track of n_i counts for each word in corpus
+        word_counts = []
 
         for page in pages:
             pid = int(page.find('id').text.strip())
@@ -108,22 +92,24 @@ class Indexer:
                     if '|' in w:
                         link_page, link_text = w.split('|', 1)
                         id2pn[pid].append(link_page.lower())
-                        self.add_to_corpus(link_text, page_info, idf_arr)
+                        self.add_to_corpus(link_text, page_info, ni_counts, word_counts)
                     elif ':' in w:
                         id2pn[pid].append(w)  # we aren't sure if category pages should count as links
                         w = w.replace(":", " ")
-                        self.add_to_corpus(w, page_info, idf_arr)
+                        self.add_to_corpus(w, page_info, ni_counts, word_counts)
                     else:  # links where the text is the name of the page
                         id2pn[pid].append(w)
-                        self.add_to_corpus(w, page_info, idf_arr)
+                        self.add_to_corpus(w, page_info, ni_counts, word_counts)
                 else:  # i.e. if the word is not a link
-                    self.add_to_corpus(word, page_info, idf_arr)
+                    self.add_to_corpus(word, page_info, ni_counts, word_counts)
 
-            self.tf_array.append(page_info[0])
+            word_counts.append(page_info[0])
             max_in_each_row.append(page_info[1])
                 
-        self.calculate_tf(max_in_each_row)
-        self.calculate_idf(idf_arr)
+        # self.calculate_tf(max_in_each_row)
+        # self.calculate_idf(idf_arr)
+
+        return IndexInfo(self.calculate_idf(ni_counts), max_in_each_row, word_counts)
 
         # could we instead make this function take in the list of pages and 
         # return idf_arr and max_in_each_row and tf_array and id2pn?
@@ -133,29 +119,55 @@ class Indexer:
         for idx, row in enumerate(self.tf_array):
             self.tf_array[idx] = [wc / maxs[idx] for wc in row]
 
-    def calculate_idf(self, n_i_list: list):
+    def calculate_idf(self, n_i_list: list) -> list:
+        """converts an n_i counts list into an IDF scores list"""
         n = len(self.page_ids)
-        self.idf_array = [math.log(n / n_i) for n_i in n_i_list]
+        return [math.log(n / n_i) for n_i in n_i_list]
     
     def calculate_relevance(self):
         for r in self.tf_array:
             self.relevance.append([elem * self.idf_array[idx] for idx, elem in enumerate(r)])
 
-    def write_files(self, title_file: str, words_file: str):
+    def make_wtr_dict(self, rel_array) -> dict:
+        """converts relevance array to a dictionary of words to dict(ids, rel)"""
         words_to_relevance = {}
 
-        # this should go in a method that takes in a relevance array and returns
-        # a words to relevance dictionary
         for col_idx, word in enumerate(self.corpus.keys()):
             ids_to_relevance = {}
-            for row_idx in range(len((self.relevance))):
+            for row_idx in range(len((rel_array))):
                 page_id = self.page_ids[row_idx][0]
-                ids_to_relevance[page_id] = self.relevance[row_idx][col_idx]
+                ids_to_relevance[page_id] = rel_array[row_idx][col_idx]
 
             words_to_relevance[word] = ids_to_relevance
 
+        return words_to_relevance
+
+    def make_tf_array(self, ii: IndexInfo) -> list:
+        """converts an IndexInfo into a term frequency array"""
+        tf = [[0 for i in range(len(ii.word_counts[0]))] for j in range(len(ii.word_counts))]
+        for idx, row in enumerate(ii.word_counts):
+            tf[idx] = [wc / ii.max_per_row[idx] for wc in row]
+
+        return tf
+
+    def make_relevance(self, ii: IndexInfo) -> list:
+        """converts an IndexInfo into a relevance scores array"""
+        rel = []
+        for r in self.make_tf_array(ii):
+            rel.append([elem * ii.idf_array[idx] for idx, elem in enumerate(r)])
+
+        return rel
+
+    def get_pages(self, xml_file: str) -> list:
+        """returns the list of pages from a given xml file"""
+        root: "Element" = et.parse(xml_file).getroot()
+        return root.findall("page")
+
+    def write_files(self, xml_file: str, title_file: str, words_file: str):
+        wtr_dict = self.make_wtr_dict(self.make_relevance(self.make_corpus(self.get_pages(xml_file))))
+
         # pass words_to_relevance into file_io to write to words file
-        file_io.write_words_file(words_file, words_to_relevance)
+        file_io.write_words_file(words_file, wtr_dict)
         # convert page_ids to dictionary and write to title file
         file_io.write_title_file(title_file, dict(self.page_ids))
 
