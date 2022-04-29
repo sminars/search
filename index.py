023969660beba1f7a26aac4efc16f5eba16a8a6e
mciethan <emaclex@gmail.com>
2022-04-28 22:10:50 -0400

    made corpus not global

--- index.py
+++ index.py
@@ -9,11 +9,12 @@ from nltk.stem import PorterStemmer
 import file_io
 
 class IndexInfo:
-    def __init__(self, idf_arr, max_per_row, word_counts, weights):
+    def __init__(self, idf_arr, max_per_row, word_counts, weights, corpus):
         self.idf_array = idf_arr
         self.max_per_row = max_per_row
         self.word_counts = word_counts
         self.weights = weights
+        self.corpus = corpus
 
 class Indexer:
     def __init__(self, args):
@@ -24,12 +25,12 @@ class Indexer:
         self.title_to_id = {} # look up page IDs by title
         self.id_to_index = {}
         self.n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
-        self.corpus = {} # keys = words, values = indices
+        # self.corpus = {} # keys = words, values = indices
         self.corpus_index = 0  
 
         self.write_files(args[0], args[1], args[2], args[3])
           
-    def add_to_corpus(self, words: str, info: list, idf: list, word_counts: list):
+    def add_to_corpus(self, words: str, info: list, idf: list, word_counts: list, corpus):
         """tokenizes, lower cases, and stems the elements of words and adds non-
         stop words to self.corpus
         
@@ -46,8 +47,8 @@ class Indexer:
         for wrd in tokens:
             if wrd.lower() not in STOP_WORDS:
                 stemmed_wrd = stemmer.stem(wrd.lower())
-                if stemmed_wrd not in self.corpus:
-                    self.corpus[stemmed_wrd] = self.corpus_index
+                if stemmed_wrd not in corpus:
+                    corpus[stemmed_wrd] = self.corpus_index
                     self.corpus_index += 1
                     # also add a new column in page row and in word_counts
                     info[0].append(1)
@@ -56,16 +57,16 @@ class Indexer:
                         row.append(0)
                 else:  # the word is in the corpus
                     # increment the word count for this word in this page
-                    info[0][self.corpus[stemmed_wrd]] += 1
+                    info[0][corpus[stemmed_wrd]] += 1
 
                     # if this is the first time we've seen this word in this row
-                    if info[0][self.corpus[stemmed_wrd]] == 1:
+                    if info[0][corpus[stemmed_wrd]] == 1:
                         # increment this word's idf n_i count
-                        idf[self.corpus[stemmed_wrd]] += 1
+                        idf[corpus[stemmed_wrd]] += 1
 
                 # update maximum value of this row
-                if info[0][self.corpus[stemmed_wrd]] > info[1]:
-                    info[1] = info[0][self.corpus[stemmed_wrd]]
+                if info[0][corpus[stemmed_wrd]] > info[1]:
+                    info[1] = info[0][corpus[stemmed_wrd]]
         
 
     def make_corpus(self, pages : list):
@@ -76,13 +77,14 @@ class Indexer:
         ni_counts = [] # keeps track of n_i counts for each word in corpus
         word_counts = []
         weights = [[0 for c in range(len(self.page_ids))] for r in range(len(self.page_ids))]
+        corpus = {}
 
         for page in pages:
             pid = int(page.find('id').text.strip())
             # title = page.find('title').text.strip()
             # self.page_ids.append((pid, title))
             id2pn[pid] = []
-            page_row = [0] * len(self.corpus)
+            page_row = [0] * len(corpus)
             # two-element list: first element is a list of word counts for the 
             # page, and the second element is the most common word's frequency
             page_info = [page_row, 0]
@@ -99,22 +101,22 @@ class Indexer:
                             weights[self.id_to_index[pid]][self.id_to_index[self.title_to_id[link_page]]] = 0.85
                             num_unique_links += 1
                             id2pn[pid].append(self.title_to_id[link_page])
-                        self.add_to_corpus(link_text, page_info, ni_counts, word_counts)
+                        self.add_to_corpus(link_text, page_info, ni_counts, word_counts, corpus)
                     elif ':' in w:
                         if w in self.title_to_id and self.title_to_id[w] not in id2pn[pid] and pid != self.title_to_id[w]:
                             weights[self.id_to_index[pid]][self.id_to_index[self.title_to_id[w]]] = 0.85
                             num_unique_links += 1
                             id2pn[pid].append(self.title_to_id[w])  # we aren't sure if category pages should count as links
                         w = w.replace(":", " ")
-                        self.add_to_corpus(w, page_info, ni_counts, word_counts)
+                        self.add_to_corpus(w, page_info, ni_counts, word_counts, corpus)
                     else:  # links where the text is the name of the page
                         if w in self.title_to_id and self.title_to_id[w] not in id2pn[pid] and pid != self.title_to_id[w]:
                             weights[self.id_to_index[pid]][self.id_to_index[self.title_to_id[w]]] = 0.85
                             num_unique_links += 1
                             id2pn[pid].append(self.title_to_id[w])
-                        self.add_to_corpus(w, page_info, ni_counts, word_counts)
+                        self.add_to_corpus(w, page_info, ni_counts, word_counts, corpus)
                 else:  # i.e. if the word is not a link
-                    self.add_to_corpus(word, page_info, ni_counts, word_counts)
+                    self.add_to_corpus(word, page_info, ni_counts, word_counts, corpus)
 
             word_counts.append(page_info[0])
             max_in_each_row.append(page_info[1])
@@ -128,7 +130,7 @@ class Indexer:
         # self.calculate_tf(max_in_each_row)
         # self.calculate_idf(idf_arr)
 
-        return IndexInfo(self.calculate_idf(ni_counts), max_in_each_row, word_counts, weights)
+        return IndexInfo(self.calculate_idf(ni_counts), max_in_each_row, word_counts, weights, corpus)
 
         # could we instead make this function take in the list of pages and 
         # return idf_arr and max_in_each_row and tf_array and id2pn?
@@ -154,8 +156,9 @@ class Indexer:
 
         rel_array = rel_and_ranks[0]
         ranks = rel_and_ranks[1]
+        corpus = rel_and_ranks[2]
 
-        for col_idx, word in enumerate(self.corpus.keys()):
+        for col_idx, word in enumerate(corpus.keys()):
             ids_to_relevance = {}
             for row_idx in range(len((rel_array))):
                 page_id = self.page_ids[row_idx][0]
@@ -201,7 +204,7 @@ class Indexer:
                 for k in range(len(self.page_ids)):
                     cur_ranks[j] += ii.weights[k][j] * prev_ranks[k]
 
-        return rel, cur_ranks
+        return rel, cur_ranks, ii.corpus
 
     def get_pages(self, xml_file: str) -> list:
         """returns the list of pages from a given xml file"""
@@ -220,7 +223,7 @@ class Indexer:
         self.title_to_id = {} # look up page IDs by title
         self.id_to_index = {}
         self.n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
-        self.corpus = {} # keys = words, values = indices
+        # self.corpus = {} # keys = words, values = indices
         self.corpus_index = 0  
 
     def write_files(self, xml_file: str, title_file: str, docs_file: str, words_file: str):
